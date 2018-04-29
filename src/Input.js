07b51571a440280c2885bb2efbdc2c78293a457c
Daniela Carey <danielafcarey@gmail.com>
2018-04-29 11:39:10 -0600

    Update Input state and App state ideas array based on user input

--- src/Input.js
+++ src/Input.js
@@ -4,19 +4,46 @@ class Input extends Component {
   constructor() {
     super()
 
-    this.state = {}
+    this.state = {
+      idea: {
+        titleInput: '',
+        bodyInput: '',
+        quality: 'swill'
+      }
+    }
+
+    this.setIdeaInput = this.setIdeaInput.bind(this);
+    this.sendIdeaToApp = this.sendIdeaToApp.bind(this);
+  }
+
+  setIdeaInput(event) {
+    this.setState({ idea: {...this.state.idea, [event.target.name]: event.target.value }}) 
+  }
+
+  sendIdeaToApp(event) {
+    event.preventDefault();
+    this.props.setIdeaInState(this.state.idea)
   }
 
 
   render() {
     return (
       <div className="Input">
-        <form>
-          <input className="title-input"></input>
-          <input className="body-input"></input>
+        <form onSubmit={ this.sendIdeaToApp } >
+          <input className="title-input"
+                 value={ this.state.titleInput } 
+                 onChange={ this.setIdeaInput }
+                 name='titleInput'
+                 />
+          <input className="body-input"
+                 value={ this.state.bodyInput } 
+                 onChange={ this.setIdeaInput }
+                 name='bodyInput'
+                 />
           <input className="submit-button"
-                  type="submit"
-                  value="Submit"></input>
+                 type="submit"
+                 value="Submit"
+                 />
         </form>
       </div>
     )
